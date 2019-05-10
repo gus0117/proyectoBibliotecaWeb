ba4a4edff258ff857fe43c9a1c16b9f029916db1
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2019-05-10 17:30:45 -0300

    Agregado DAO de editorial y pagina no funcional

---
+++ src/java/aplicacion/dao/IEditorialDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.dao;
+
+import aplicacion.modelo.dominio.Editorial;
+import java.util.List;
+
+/**
+ *
+ * @author Rocio
+ */
+public interface IEditorialDAO {
+    public void agregarEditorial(Editorial editorial);
+    public void borrarEditorial(Editorial editorial);
+    public Editorial obtenerEditorial(Integer codigo);
+    public List<Editorial> obtenerEditoriales();
+}
